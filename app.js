1ef2ab927b063a2d88bf144bd78354d7a8213288
imranariffin <imran.ariffin@mail.utoronto.ca>
2015-06-21 13:36:45 -0400

    added a dummy route for other apis

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/weather', getWeather);
+app.use('/other-api'. function (req, res, next) {
+  res.send("more api's coming");
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
